52822d095b01266bb521ca038d3a2e0f2876ad06
wecoastca <foxconq@gmail.com>
2020-04-18 19:13:25 +0300

    Counting coordinates as spiral

--- packages/web/components/Scene/Scene.tsx
+++ packages/web/components/Scene/Scene.tsx
@@ -44,15 +44,9 @@ class Scene extends React.Component <Props & StoreProps, State> {
       renderer.render(scene, camera);
     };
 
-    const fbxLoader = new FBXLoader();
-
-    const previousModel = scene.getObjectByName('initModel');
-    const vector = new THREE.Vector3();
-    const prevModelPosition = previousModel.getWorldPosition(vector);
-
     const makeCircleModel = (numberOfFlowers) => {
-      const geometry = new THREE.CircleGeometry(100 + numberOfFlowers * 10);
-      const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
+      const geometry = new THREE.CircleGeometry(10 + numberOfFlowers * 10, numberOfFlowers);
+      const material = new THREE.MeshBasicMaterial({ color: 0x808080 });
       const circle = new THREE.Mesh(geometry, material);
       circle.scale.set(0.01, 0.01, 0.01);
       circle.position.set(0, 1.5, 0);
@@ -61,16 +55,44 @@ class Scene extends React.Component <Props & StoreProps, State> {
       scene.add(circle);
     };
 
-    makeCircleModel(flowNum);
+    const estimateBaseTrajectory = (numberOfFlowers:Number, source: string) => {
+      const circleModel = scene.getObjectByName('circle');
+      const circleModelPosition = circleModel.position;
 
-    const estimateBaseTrajectory = (numberOfFlowers:Number) => {
-      const a = 0;
-      const K = a / 2 * Math.PI;
-      const u = 0;
+      const calculateCoordinates = (spiralStep, angle) => ({
+        x: (spiralStep / 2 * Math.PI) * angle * Math.cos(angle),
+        z: (spiralStep / 2 * Math.PI) * angle * Math.sin(angle),
+      });
 
-      const x = Math.abs(K * u * Math.cos(u));
-      const y = Math.abs(K * u * Math.sin(u));
+      const calculateRotation = () => {
+
+      };
+
+      for (let i = 0; i < numberOfFlowers; i++) {
+        const xCoordinate = calculateCoordinates(0.05, i * Math.PI / 8).x;
+        const zCoordinate = calculateCoordinates(0.05, i * Math.PI / 8).z;
+
+        fbxLoader.load(source,
+          (e) => {
+            e.rotation.set(0, 0, 0);
+            e.position.set(circleModelPosition.x + xCoordinate, circleModelPosition.y, circleModelPosition.z + zCoordinate);
+            e.scale.set(0.03, 0.03, 0.03);
+
+            scene.add(e);
+          });
+      }
     };
+
+    const fbxLoader = new FBXLoader();
+
+    makeCircleModel(flowNum);
+    estimateBaseTrajectory(flowNum, url);
+
+    const previousModel = scene.getObjectByName('initModel');
+    scene.remove(previousModel);
+
+    animate();
+
     // fbxLoader.load('public/models/5roses/5_roses.fbx',
     //   (e) => {
     //     e.rotation.set(0,0,0);
@@ -78,20 +100,6 @@ class Scene extends React.Component <Props & StoreProps, State> {
     //     e.scale.set(0.00001, 0.00001, 0.00001);
     //     scene.add(e);
     //   });
-
-
-    // for (let i = 0; i < flowNum; i++) {
-    //   fbxLoader.load(url,
-    //     (e) => {
-    //       e.rotation.set(0,0,0);
-    //       e.position.set(0+i,0,0);
-    //       e.scale.set(0.03, 0.03, 0.03);
-    //       scene.add(e);
-    //     });
-    // }
-    scene.remove(previousModel);
-
-    animate();
   }
 
   stateListenToGatherBouquet = () => {
